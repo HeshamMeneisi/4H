7ebd32b8bcda4430ba4ccc7fa14f13f0077b2b2c
Nader Alsharkawy <naderalsharkawy@gmail.com>
2016-12-27 22:30:21 +0200

    Update profile

--- hud.php
+++ hud.php
@@ -1,9 +1,12 @@
+<head>
 <link rel="stylesheet" href="css/hud.css">
 <link rel="stylesheet" href="css/form.css">
 <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+<link rel="stylesheet" href="css/profile.css">
 <script src="js/auth.js"></script>
 <script src="js/form.js"></script>
 <script src="js/jquery-3.1.1.js"></script>
+</head>
 <div class='uhud'>
 <a href="../"><img class="logo" src="files/logo.png" alt="Socio"/></a>
 <?php
--- profile.php
+++ profile.php
@@ -4,7 +4,6 @@
 <?php
 require_once 'user.php';
 include_once 'db.php';
-
 if (isset($_GET['uid'])) {
     $uid = $_GET['uid'];
     $st = $pdo->prepare('SELECT * FROM user WHERE BINARY uid=:uid');
@@ -30,31 +29,28 @@ if (!isset($user['photo']) || !$user['photo'] || !file_exists($pic)) {
 $_GET['p'] = 1;
 include_once 'hud.php';
 ?>
-<link rel="stylesheet" href="css/profile.css">
-
-<div class="nickname">
-  <?php echo $user['nickname']; ?>
-</div>
-
+<div class='profile-head'>
 <div class="name">
   <?php echo $user['fname'].' '.$user['lname']; ?>
 </div>
-
-<div class="img">
-  <a target="_blank" href="image/no_profile_pic.jpg">
-    <img src=<?php echo $pic; ?>  width="300" height="200">
-  </a>
-  <div class="addpp">
-    <a href="#">
-    <?php if ($nopic):?>
-    Add a Picture
-  <?php else:?>
-    Change Picture
-  <?php endif; ?>
-    </a>
-  </div>
+<div class="nickname">
+  <?php echo '('.$user['nickname'].')'; ?>
 </div>
-
+<div class="profile-picture">
+<table id="pp">
+<tr>
+    <td>
+    <a target="_blank" href="image/no_profile_pic.jpg"><img src=<?php echo $pic; ?> /></a>
+    </td>
+</tr>
+<tr>
+    <td id="pp-link">
+        <a href="#"><?php if ($nopic):?>Add a Picture<?php else:?>Change Picture<?php endif; ?></a>
+    </td>
+</tr>
+</table>
+</div>
+</div>
 <?php
 $_GET['p'] = 1;
 include 'timeline.php';
