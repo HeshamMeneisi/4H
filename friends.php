70d59a63d382d4912e1a334748926a1d1dd8f155
Hesham Meneisi <heshammeneisi@gmail.com>
2016-12-27 03:35:06 +0200

    Set a template for friends.php behaviour.

--- friends.php
+++ friends.php
@@ -1,1 +1,23 @@
-<!-- display all friends -->
+<!-- display all requests if $_GET['r'] and display all friends if $_GET['l'] -->
+<?php
+include_once 'db.php';
+include_once 'user.php';
+
+if (is_logged()) {
+    if (isset($_GET['r'])) {
+        $uid = get_user()['uid'];
+        // display all friend requests
+    }
+}
+
+if (isset($_GET['l'])) {
+    if (isset($_GET['uid'])) {
+        $uid = $_GET['uid'];
+    } elseif (is_logged()) {
+        $uid = get_user()['uid'];
+    } else {
+        echo 'Invalid request.';
+        die();
+    }
+    // display friend list of $uid
+}
