df8b56cbed567427d9d3e0f5ee15d5f75dadf087
Hesham Meneisi <heshammeneisi@gmail.com>
2016-12-27 22:56:00 +0200

    Set searching convention.

--- userhud.php
+++ userhud.php
@@ -2,20 +2,20 @@
 include_once 'user.php';
 
 ?>
-<?php 
-    if (isset($_GET['p'])){
+<?php
+    if (isset($_GET['p'])) {
         echo '<ul class="navbar-p">';
-    }
-    else{
+    } else {
         echo '<ul class="navbar">';
     }
 ?>
   <li><form method="get" action="search.php" id="search">
-   <input name="sr" type="text" size="40" placeholder="Search..." />
+    <input type="hidden" name="mode" value="q">
+    <input name="query" type="text" size="40" placeholder="Search..." />
   </form>
   </li>
   <li><a href="index.php">Home</a></li>
-    <li class="notify-btn"><a href="#">Notifications<span class="notify pink">2</span></a></li>    
+    <li class="notify-btn"><a href="#">Notifications<span class="notify pink">2</span></a></li>
     <?php if (isset($_GET['p'])): ?>
   <li><a href="#">Edit Info</a></li>
   <li><a href="friends.php">Friends</a></li>
