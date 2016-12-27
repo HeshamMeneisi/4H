4dec58bce4f000cef4e0ca0ec44fa6e470da2b3e
Nader Alsharkawy <naderalsharkawy@gmail.com>
2016-12-27 03:42:07 +0200

    Finalize posts and comments view mode

--- comment.php
+++ comment.php
@@ -1,7 +1,113 @@
 <!--  view and submit modes should be implemented, $_GET['mode']={'v','s'} -->
 <?php
-if ($_GET['mode'] == 'v') {
-    // view comment with id = $_GET['id'] and make sure to display number of likes
-} else {
-    // display the comment form, the commenting operation should be handled in ajax
+include 'db.php';
+
+// Handle invalid invoking
+
+if (!isset($_GET['mode']) || !isset($_GET['id'])) {
+	echo "Error retrieving comment.";
+}
+else {
+	if ($_GET['mode'] == 'v') {
+
+		// view comment with id = $_GET['id'] and make sure to display number of likes
+
+		if (isset($_GET['id'])) {
+			$comments = fetch_comments($_GET['id'], $pdo);
+			if ($comments) {
+				echo '<br /><br /><strong>Comments</strong>';
+				foreach($comments as $comment) {
+					$commenter = fetch_commenter_name($comment['cuid'], $pdo);
+					$commenter_name = $commenter['fname'] . ' ' . $commenter['lname'] . ' (' . $commenter['nickname'] . ')';
+					$comment_time = $comment['ctime'];
+					$comment_content = $comment['caption'];
+					echo '<br/><br/>' . $commenter_name . ' at ' . $comment_time;
+					$fetched_comment_likes = fetch_comment_likes($comment['cid'], $pdo);
+					echo '<br />' . $comment_content;
+					if ($fetched_comment_likes) {
+						$comment_likes = $fetched_comment_likes->fetchAll(PDO::FETCH_ASSOC);
+
+						// One like
+
+						if ($fetched_comment_likes->rowCount() == 1) {
+							$liker = fetch_name($comment_likes[0]['uid'], $pdo);
+							echo '<br/>' . $liker['fname'] . ' ' . $liker['lname'] . ' likes this comment.';
+						}
+
+						// Two likes
+
+						elseif ($fetched_comment_likes->rowCount() == 2) {
+							$first_liker = fetch_name($comment_likes[0]['uid'], $pdo);
+							$second_liker = fetch_name($comment_likes[1]['uid'], $pdo);
+							echo '<br/>' . $first_liker['fname'] . ' ' . $first_liker['lname'] . ' and ' . $second_liker['fname'] . ' ' . $second_liker['lname'] . ' like this comment.';
+						}
+
+						// More than two likes
+
+						else {
+							$liker = fetch_name($comment_likes[0]['uid'], $pdo);
+							echo '<br/>' . $liker['fname'] . ' ' . $liker['lname'] . ' and ' . ($fetched_comment_likes->rowCount() - 1) . ' others like this comment.';
+						}
+					}
+					else {
+						echo '<br/>No likes yet';
+					}
+				}
+			}
+		}
+	}
+	else {
+
+		// display the comment form, the commenting operation should be handled in ajax
+
+	}
+}
+
+// This function returns the name fields fetched via user id
+
+function fetch_commenter_name($user_id, $pdo)
+{
+	$name_fetcher = $pdo->prepare('SELECT fname,lname,nickname FROM user WHERE uid=:uid');
+	if (!$name_fetcher->execute(array(
+		':uid' => $user_id
+	))) {
+		throw new Exception('DB connection failed.');
+	}
+	elseif ($name_fetcher->rowCount() == 1) {
+		return $name_fetcher->fetch(PDO::FETCH_ASSOC);
+	}
+}
+
+// This function returns the comments of a post fetched via post id
+
+function fetch_comments($post_id, $pdo)
+{
+	$comments_fetcher = $pdo->prepare('SELECT * FROM comment WHERE pid=:pid');
+	if (!$comments_fetcher->execute(array(
+		':pid' => $post_id
+	))) {
+		throw new Exception('DB connection failed.');
+	}
+	elseif ($comments_fetcher->rowCount() > 0) {
+		return $comments_fetcher->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	return null;
+}
+
+// This function returns the likes of a post fetched via post id
+
+function fetch_comment_likes($comment_id, $pdo)
+{
+	$comment_likes_fetcher = $pdo->prepare('SELECT uid FROM likes_comment WHERE cid=:cid');
+	if (!$comment_likes_fetcher->execute(array(
+		':cid' => $comment_id
+	))) {
+		throw new Exception('DB connection failed.');
+	}
+	elseif ($comment_likes_fetcher->rowCount() > 0) {
+		return $comment_likes_fetcher;
+	}
+
+	return null;
 }
--- post.php
+++ post.php
@@ -1,87 +1,126 @@
 <!--  view and submit modes should be implemented, $_GET['mode']={'v','s'} -->
 <?php
-
 include 'db.php';
 
-if ($_GET['mode'] == 'v') {
-    // view post with id = $_GET['id'] and make sure to display number of likes and an expandable comments section
-    if (isset($_GET['id'])){
-        //Fetch post data
-        $the_post = fetch_post($_GET['id'],$pdo);
-        $name = fetch_name($the_post['puid'],$pdo);
-        $fetched_likes = fetch_likes($the_post['pid'],$pdo);
-        $likes = $fetched_likes->fetchAll(PDO::FETCH_ASSOC);
-        
-        //Display name and post time
-        echo $name['fname']." ".$name['lname']." (".$name['nickname'].")";
-        echo " at ".$the_post['ptime'];
-        
-        //No likes
-        if(!$fetched_likes){
-            echo '<br><br> 0 likes';
-        }
-        
-        //One like
-        elseif($fetched_likes->rowCount()==1){
-            $liker = fetch_name($likes['uid'],$pdo);
-            echo "<br><br>".$liker['fname']." ".$liker['lname']." likes this.";
-        }
-        
-        //Two likes
-        elseif($fetched_likes->rowCount()==2){
-            $first_liker = fetch_name($likes[0]['uid'],$pdo);
-            $second_liker = fetch_name($likes[1]['uid'],$pdo);
-            echo "<br><br>".$first_liker['fname']." ".$first_liker['lname']." and ".$second_liker['fname']." ".$second_liker['lname']." like this.";
-        }
-        
-        //More than two likes
-        else{
-                $liker = fetch_name($likes[0]['uid'],$pdo);
-                echo "<br><br>".$liker['fname']." ".$liker['lname']." and ".($fetched_likes->rowCount()-1)." others like this.";
-        }
-        }
-        echo "<br><br>".$the_post['caption'];
-        include 'comment.php';
+// Handle invalid invoking
+
+if (!isset($_GET['mode']) || !isset($_GET['id'])) {
+	echo "Error retrieving post.";
 }
 else {
-    // display the post form, the posting operation should be handled in ajax
-    //Text area
-    echo "<form><textarea id=\"post_text\" rows=\"10\" cols=\"80\"></textarea></form>";
-    //Submit button
-    echo "<button id=\"submitPost\">Post</button>";
+	if ($_GET['mode'] == 'v') {
+
+		// view post with id = $_GET['id'] and make sure to display number of likes and an expandable comments section
+
+		if (isset($_GET['id'])) {
+
+			// Fetch post data
+
+			$the_post = fetch_post($_GET['id'], $pdo);
+			$name = fetch_name($the_post['puid'], $pdo);
+			$poster_name = $name['fname'] . ' ' . $name['lname'];
+			$nickname = ' (' . $name['nickname'] . ') ';
+			$caption = $the_post['caption'];
+			$time = $the_post['ptime'];
+			$fetched_likes = fetch_likes($_GET['id'], $pdo);
+
+			// Display poster name and post time
+			echo $poster_name . $nickname . 'posted at ' . $time . '<br/>' . $caption;
+
+			// Check for post likes
+
+			if ($fetched_likes) {
+				$likes = $fetched_likes->fetchAll(PDO::FETCH_ASSOC);
+
+				// One like
+
+				if ($fetched_likes->rowCount() == 1) {
+					$liker = fetch_name($likes['uid'], $pdo);
+					echo '<br />' . $liker['fname'] . ' ' . $liker['lname'] . ' likes this.';
+				}
+
+				// Two likes
+
+				elseif ($fetched_likes->rowCount() == 2) {
+					$first_liker = fetch_name($likes[0]['uid'], $pdo);
+					$second_liker = fetch_name($likes[1]['uid'], $pdo);
+					echo '<br/>' . $first_liker['fname'] . ' ' . $first_liker['lname'] . ' and ' . $second_liker['fname'] . ' ' . $second_liker['lname'] . ' like this.';
+				}
+
+				// More than two likes
+
+				else {
+					$liker = fetch_name($likes[0]['uid'], $pdo);
+					echo '<br/>' . $liker['fname'] . ' ' . $liker['lname'] . ' and ' . ($fetched_likes->rowCount() - 1) . ' others like this.';
+				}
+			}
+
+			// No likes
+
+			else {
+				echo '<br/>No likes yet';
+			}
+		}
+
+		include 'comment.php';
+
+	}
+	else {
+
+		// display the post form, the posting operation should be handled in ajax
+		// Text area
+
+		echo '<form><textarea id=\'post_text\' rows=\'10\' cols=\'80\'></textarea></form>';
+
+		// Submit button
+
+		echo '<button id=\'submitPost\'>Post</button>';
+	}
 }
 
-//This function returns a post fetched via post id
-function fetch_post($post_id,$pdo){
-    $post_fetcher = $pdo->prepare('SELECT * FROM post WHERE pid=:pid');
-    if (!$post_fetcher->execute(array(':pid' => $post_id))) {
-        throw new Exception('DB connection failed.');
-    } 
-    elseif ($post_fetcher->rowCount()==1) {
-        return $post_fetcher->fetch(PDO::FETCH_ASSOC);
-    }
-    
+// This function returns a post fetched via post id
+
+function fetch_post($post_id, $pdo)
+{
+	$post_fetcher = $pdo->prepare('SELECT * FROM post WHERE pid=:pid');
+	if (!$post_fetcher->execute(array(
+		':pid' => $post_id
+	))) {
+		throw new Exception('DB connection failed.');
+	}
+	elseif ($post_fetcher->rowCount() == 1) {
+		return $post_fetcher->fetch(PDO::FETCH_ASSOC);
+	}
 }
 
-//This function returns the name fields fetched via user id
-function fetch_name($user_id,$pdo){
-    $name_fetcher = $pdo->prepare('SELECT fname,lname,nickname FROM user WHERE uid=:uid');
-    if (!$name_fetcher->execute(array(':uid' => $user_id))) {
-        throw new Exception('DB connection failed.');
-    } 
-    elseif ($name_fetcher->rowCount()==1) {
-        return $name_fetcher->fetch(PDO::FETCH_ASSOC);
-    }
+// This function returns the name fields fetched via user id
+
+function fetch_name($user_id, $pdo)
+{
+	$name_fetcher = $pdo->prepare('SELECT fname,lname,nickname FROM user WHERE uid=:uid');
+	if (!$name_fetcher->execute(array(
+		':uid' => $user_id
+	))) {
+		throw new Exception('DB connection failed.');
+	}
+	elseif ($name_fetcher->rowCount() == 1) {
+		return $name_fetcher->fetch(PDO::FETCH_ASSOC);
+	}
 }
 
-//This function returns the likes of a post fetched via post id
-function fetch_likes($post_id,$pdo){
-    $fetched_likes_fetcher = $pdo->prepare('SELECT uid FROM likes_post WHERE pid=:pid');
-    if (!$fetched_likes_fetcher->execute(array(':pid' => $post_id))) {
-        throw new Exception('DB connection failed.');
-    } 
-    elseif ($fetched_likes_fetcher->rowCount()>0) {
-        return $fetched_likes_fetcher;
-    }
-    return null;
+// This function returns the likes of a post fetched via post id
+
+function fetch_likes($post_id, $pdo)
+{
+	$fetched_likes_fetcher = $pdo->prepare('SELECT uid FROM likes_post WHERE pid=:pid');
+	if (!$fetched_likes_fetcher->execute(array(
+		':pid' => $post_id
+	))) {
+		throw new Exception('DB connection failed.');
+	}
+	elseif ($fetched_likes_fetcher->rowCount() > 0) {
+		return $fetched_likes_fetcher;
+	}
+
+	return null;
 }
