248b424166affd9cc660d9aecc1102b17cc404f8
Hesham Meneisi <heshammeneisi@gmail.com>
2016-12-29 14:53:43 +0200

    Fix a typo in person.php

--- person.php
+++ person.php
@@ -5,13 +5,12 @@ $link = './profile.php?uid='.$person['uid'];
 $person_name = $person['fname'].' '.$person['lname']." (<a href={$link}>".$person['nickname'].'</a>)';
 if (isset($person['_time'])) {
     $person_time = $person['_time'];
-    
 } else {
     $person_time = null;
 }
 $uid = $person['uid'];
 $pic = 'content/users/'.$uid.'/profile_picture.png';
 if (!file_exists($pic)) {
-    $pic = "./content/static/default_picture/{$user['gender']}.png";
+    $pic = "./content/static/default_picture/{$person['gender']}.jpg";
 }
-echo "<img class='request_thumb' src='{$pic}' height='50' width='50'>".$person_name.($person_time ? '<br>Request time: '.date('l, F jS, Y', strtotime($person_time)): '');
+echo "<img class='request_thumb' src='{$pic}' height='50' width='50'>".$person_name.($person_time ? '<br>Request time: '.date('l, F jS, Y', strtotime($person_time)) : '');
